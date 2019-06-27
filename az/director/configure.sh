5695ed4cb38a55a91fd330dafeaeed255e7b2a17
vponnam <vponnam>
2019-06-27 14:58:57 -0400

    test

--- az/director/configure.sh
+++ az/director/configure.sh
@@ -2,6 +2,8 @@
 
 set -eu
 
-vault read --field=value concourse/main/${USER}/om-ssh-key > om-ssh-key
+
+
+vault read --field=value concourse/main/om-ssh-private > om-ssh-key
 bosh int <(echo "${Director_Prop}") --var-file ssh-key=om-ssh-key > dir-params.yml
 om --env om-configs/env.yml configure-director -c src/az/director/director.yml --vars-file dir-params.yml
