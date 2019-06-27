fa5d743e845932489233a70746acd5e3f10bbeb2
vponnam <vponnam>
2019-06-27 15:11:23 -0400

    test it

--- az/director/configure.sh
+++ az/director/configure.sh
@@ -2,6 +2,7 @@
 
 set -exu
 
-vault read --field=value concourse/main/om-ssh-private > om-configs/om-ssh-key
-bosh int <(echo "${Director_Prop}") --var-file ssh-key=om-configs/om-ssh-key > dir-params.yml
-om --env om-configs/env.yml configure-director -c src/az/director/director.yml --vars-file dir-params.yml
+vault read --field=value concourse/main/om-ssh-private > om-ssh-key
+bosh int secrets.yml --var-file ssh-key=om-ssh-key > keys.yml
+bosh int <(echo "${Director_Prop}") > dir-params.yml
+om --env om-configs/env.yml configure-director -c src/az/director/director.yml --vars-file dir-params.yml --vars-file keys.yml
