9902c1ec4a63116f1642d02b4d2cebb8ec7f738f
vponnam <vponnam>
2019-06-27 15:36:43 -0400

    fix ssh placeholder

--- az/director/configure.sh
+++ az/director/configure.sh
@@ -3,6 +3,6 @@
 set -exu
 
 vault read --field=value concourse/main/om-ssh-private > om-ssh-key
-bosh int src/az/director/secrets.yml --var-file ssh-key=om-ssh-key > keys.yml
+bosh int src/az/director/secrets.yml --var-file om-ssh-key=om-ssh-key > keys.yml
 bosh int <(echo "${Director_Prop}") > dir-params.yml
-om --env om-configs/en.yml configure-director -c src/az/director/director.yml --vars-file dir-params.yml --vars-file keys.yml
+om --env om-configs/env.yml configure-director -c src/az/director/director.yml --vars-file dir-params.yml --vars-file keys.yml
