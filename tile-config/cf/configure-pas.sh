2a29479a2397d0011e0533f17b5ab33079494fdf
vponnam <vponnam>
2019-07-11 11:21:15 -0400

    test it

--- tile-config/cf/configure-pas.sh
+++ tile-config/cf/configure-pas.sh
@@ -7,4 +7,5 @@ bosh int src/tile-config/cf/secrets.yml --var-file wildcard-cert=cert --var-file
 bosh int <(echo "${cf-vars}") > cf-vars.yml
 bosh int <(echo "${OM_ENV}") > env.yml
 
+om --env env.yml stage-product -p cf -v ${pas_version}
 om --env env.yml configure-product -c src/tile-config/cf/cf-properties.yml -l cf-vars.yml -l cf-vars.yml -l keys.yml
