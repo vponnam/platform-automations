db9250aa02b9e6399595d9b31e85e62d610af548
vponnam <vponnam>
2019-07-11 16:38:16 -0400

    test

--- tile-config/cf/configure-pas.sh
+++ tile-config/cf/configure-pas.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -xu
+set -eu
 
 vault read --field=value concourse/main/vponnam/pas-cert > cert
 vault read --field=value concourse/main/vponnam/pas-key > key
@@ -10,4 +10,6 @@ bosh int <(echo "${cf_vars}") > cf-vars.yml
 bosh int <(echo "${OM_ENV}") > env.yml
 
 om --env env.yml stage-product -p cf -v ${pas_version}
-om --env env.yml configure-product -c src/tile-config/cf/cf-properties.yml  --vars-file src/tile-config/cf/cf-vars.yml  --vars-file cf-vars.yml  --vars-file keys.yml
+
+bosh int src/tile-config/cf/cf-vars.yml --var-file cf-vars.yml --var-file keys.yml > vars.yml
+om --env env.yml configure-product -c src/tile-config/cf/cf-properties.yml  --vars-file vars.yml
